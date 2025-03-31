7d139e1b25afea1c25c301aa6ad7f853ea71ca7a
witchbladedovi4 <pidzes@vk.com>
2025-03-31 14:50:08 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Figure
+{
+    public double Volume(double a)
+    {
+        return Math.Pow(a, 3);
+    }
+
+    public double Volume(double a, double b, double c)
+    {
+        return a * b * c;
+    }
+
+    public static void Main(string[] args)
+    {
+        Figure figure = new Figure();
+
+        double cubeSide = 5;
+        double cubeVolume = figure.Volume(cubeSide);
+        Console.WriteLine($"Объем куба со стороной {cubeSide} = {cubeVolume}");
+
+        double a = 2;
+        double b = 3;
+        double c = 4;
+        double parallelepipedVolume = figure.Volume(a, b, c);
+        Console.WriteLine($"Объем прямоугольного параллелепипеда со сторонами {a}, {b}, {c} = {parallelepipedVolume}");
+    }
+}
